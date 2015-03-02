9e8d7c22bf8b90b9791cb16c7af146979021b1f4
geekerzp <geekerzp@gmail.com>
2015-03-03 00:24:45 +0800

    add lib files

--- binary-search-tree.lisp
+++ binary-search-tree.lisp
@@ -16,8 +16,18 @@
                  :r (node-r bst))
                 (make-node
                  :elt elt
-                 :r (bst-insert obj (node-r bst) >)
+                 :r (bst-insert obj (node-r bst) <)
                  :l (node-l bst)))))))
+
+(defun bst-find (obj bst <)
+  (if (null bst)
+      nil
+      (let ((elt (node-elt bst)))
+        (if (eql obj elt)
+            bst
+            (if (funcall < obj elt)
+                (bst-find obj (node-l bst) <)
+                (bst-find obj (node-r bst) <))))))
 
 (defun bst-min (bst)
   (and bst
@@ -26,3 +36,51 @@
 (defun bst-max (bst)
   (and bst
        (or (bst-max (node-r bst)) bst)))
+
+(defun bst-remove (obj bst <)
+  (if (null bst)
+      nil
+      (let ((elt (node-elt bst)))
+        (if (eql obj elt)
+            (percolate bst)
+            (if (funcall < obj elt)
+                (make-node
+                 :elt elt
+                 :l (bst-remove obj (node-l bst) <)
+                 :r (node-r bst))
+                (make-node
+                 :elt elt
+                 :l (node-l bst)
+                 :r (bst-remove obj (node-r bst) <)))))))
+
+(defun percolate (bst)
+  (let ((l (node-l bst)) (r (node-r bst)))
+    (cond ((null l) r)
+          ((null r) l)
+          (t (if (zerop (random 2))
+                 (make-node :elt (node-elt (bst-max l))
+                            :r r
+                            :l (bst-remove-max l))
+                 (make-node :elt (node-elt (bst-min r))
+                            :r (bst-remove-min r)
+                            :l l))))))
+
+(defun bst-remove-max (bst)
+  (if (null (node-r bst))
+      (node-l bst)
+      (make-node :elt (node-elt bst)
+                 :l   (node-l bst)
+                 :r   (bst-remove-max (node-r bst)))))
+
+(defun bst-remove-min (bst)
+  (if (null (node-l bst))
+      (node-r bst)
+      (make-node :elt (node-elt bst)
+                 :l   (bst-remove-min (node-l bst))
+                 :r   (node-r bst))))
+
+(defun bst-traverse (fn bst)
+  (when bst
+    (bst-traverse fn (node-l bst))
+    (funcall fn (node-elt bst))
+    (bst-traverse fn (node-r bst))))
