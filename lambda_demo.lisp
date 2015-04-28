024c49e90c66eb72488124b6fdae0acba503e5b4
geekerzp <geekerzp@gmail.com>
2015-04-28 22:58:00 +0800

    add lambda demo

---
+++ lambda_demo.lisp
@@ -0,0 +1,27 @@
+(setf note-table
+      '((c . 1)
+        (c-sharp . 2)
+        (d . 3)
+        (d-sharp . 4)
+        (e . 5)
+        (f . 6)
+        (f-sharp . 7)
+        (g . 8)
+        (g-sharp . 9)
+        (a . 10)
+        (a-sharp . 11)
+        (b . 12)))
+
+(defun numbers (x)
+  (mapcar #'(lambda (e)
+              (cdr (assoc e note-table)))
+          x))
+
+(defun notes (x)
+  (mapcar #'(lambda (e)
+              (car (rassoc e note-table)))
+          x))
+
+(defun raise (n x)
+  (mapcar #'(lambda (e) (+ e n))
+          x))
